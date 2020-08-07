f334f775f642bd66fa534a2cd2b10e77c4647e33
show <show75@ezweb.ne.jp>
2020-08-08 01:40:40 +0900

    update(core): Kadai01

--- Kadai02/src/StringCount.java
+++ Kadai02/src/StringCount.java
@@ -1,11 +1,6 @@
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.MatchResult;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCount {
@@ -13,11 +8,12 @@ public class StringCount {
 	public static final int ABNORMAL = -1;	
 	public static final int FRONT = 0;	
 	public static final int BACK = 1;
+	public static final int LENGTH = 5;	
 	public static final String INFILE = "/Users/sakaishow/workspace/test.txt";
 	public static final String SEARCH_WORD = "広島";
 	public static final String CHAR_CODE = "MS932";
 	public static final String REMOVE_REGEX = "[\\p{C} 　]";
-	public static final String WORD_REGEX = "(.{0,5}" + SEARCH_WORD + ").{0,5}";
+	public static final String WORD_REGEX = "(.{0," + LENGTH + "}" + SEARCH_WORD + ").{0," + LENGTH + "}";
 	public static final String W001 = "入出力エラーが発生しました。%sにファイルが存在しない可能性があります。\n";
 	public static final String I001 = "プログラムを終了します";
 	public static final String NANMONAI = "";
@@ -25,35 +21,28 @@ public class StringCount {
 	
 	public static void main(String[] args) {
 		String all = NANMONAI;
+		
 		try {
 			all = Files.readString(Path.of(INFILE));
 		} catch (IOException e) {
 			System.out.printf(W001 + I001, INFILE);
 			System.exit(ABNORMAL);
 		}
+		
 		all = all.replaceAll(REMOVE_REGEX, NANMONAI);
-		List<String> results = new ArrayList<String>();
 		
-		// Streamを使うパターン
-//		Integer count = 0;
-//		Pattern.compile(WORD_REGEX)
-//			.matcher(all)
-//			.results()
-//			.forEach((MatchResult result) -> {
-//				results.add(result.group());
-//			});
-		
-		// Whileを使うパターン
-		int end = 0;
 		Pattern p1 = Pattern.compile(WORD_REGEX);
-		Matcher m1 = p1.matcher(all);
-		while(m1.find(end)) {
-			results.add(m1.group());
-			end = m1.end(1);
-		}
-		
-		results.forEach(result -> {
-			System.out.println(result);
-		});
+		Pattern.compile("[、。]")
+			.splitAsStream(all)
+			.map(p1::matcher)
+			.forEach(x -> {
+				int end = 0;
+				while(x.find(end)) {
+					String[] res = x.group().split(SEARCH_WORD);
+					System.out.print(res[FRONT] + FORMAT);
+					if (res.length == 2) System.out.println(res[BACK]);
+					end = x.end(1);
+				}
+			});
 	}
 }
